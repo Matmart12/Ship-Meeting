d73c316059640833f3c43dc21b6d2c377d73bc00
Matmart12 <mathis.m7878@gmail.com>
2024-05-14 16:46:52 +0200

    changements mineurs

--- formules.php
+++ formules.php
@@ -6,7 +6,7 @@ session_start();
 <head>
 <link rel="stylesheet" href="all.css">
 </head>
-
+<div id=divdroit><a href="accueuil.php"> <img src="https://www.educol.net/coloriage-maison-dl28263.jpg" width="40px" alt="" ></a></div>
 <table width=30% align="center" id=table>
     <tr height=100px>
         <th bgcolor="red">Formule inscrit/e</th>
