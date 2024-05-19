71ff72bf32c3dfebb2ef436e168317bb57f6276a
nismori <110066178+nismori@users.noreply.github.com>
2024-05-19 19:48:58 +0200

    Add files via upload

--- projet/php/inscription.php
+++ projet/php/inscription.php
@@ -1,11 +1,120 @@
 <?php
-$prenom = $_POST["prenom"];
-$nom = $_POST["nom"];
-$email = $_POST["email"];
-$age = $_POST["age"];
-$pays = $_POST["pays"];
-$photo = $_FILES["photo"];
-$password = $_POST["password"] 
-$compte = fopen("/workspaces/projets/Preing 2/projet/donnees/comptes.txt","w");
-fwrite($compte,"$prenom $nom $email $age $pays $password");
-fclose($compte);
+
+if(isset($_POST['email'])){
+    $email = $_POST['email'];
+    $file_path = "../data/info_formulaire.json";
+    if(file_exists($file_path)){
+        $json_data = file_get_contents($file_path);
+        $tab = json_decode($json_data, true);
+        if(empty($json_data) || !is_array($tab)){
+            $tab = array();
+        }
+        else{
+            foreach ($tab as $compte){
+                if(isset($compte['email']) && $compte['email'] === $email){
+                    echo "L'email est déjà enregistré. Veuillez utiliser un autre email.";
+                    exit();
+                }
+            }
+        }
+    }
+} 
+if(isset($_POST['password'], $_POST['prenom'], $_POST['nom'], $_POST['age'], $_POST['pays'])){
+    $password = $_POST['password'];
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $age = $_POST['age'];
+    $pays = $_POST['pays'];
+    $file = $_FILES['photo'];
+
+    $data_array = array(
+        'email' => $email,
+        'password' => $password,
+        'prenom' => $prenom,
+        'nom' => $nom,
+        'age' => $age,
+        'pays' => $pays,
+        'ship' => 0,
+    );
+
+    if(isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+        $file = $_FILES['photo'];
+        $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $photo_name = $email . '.' . $extension;
+        move_uploaded_file($file['tmp_name'], "../icones/$photo_name");
+        $data_array['photo'] = $photo_name;
+    } 
+    else {
+        $data_array['photo'] = "0.png";
+    }
+
+    $tab[] = $data_array;
+
+    file_put_contents($file_path, json_encode($tab,JSON_PRETTY_PRINT));
+
+    header("Location: connexion.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/inscription.css">
+    <title>Formulaire d'inscription</title>
+</head>
+
+<body>
+    <form enctype="multipart/form-data" method="POST" action="inscription.php">    <div id=divdroit><a href="../page_connexion.php"> <img src="https://www.educol.net/coloriage-maison-dl28263.jpg" width="40px" alt="" ></a></div>
+        <legend><h1>Formulaire d'inscription</h1>
+            <table>  
+                <tr>
+                    <td colspan="2"><input placeholder="email" type="email" name="email" required></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input id="password" placeholder="password" type="password" name="password" required></button></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input placeholder="prenom" type="text" name="prenom" required> </td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input placeholder="nom" type="text" name="nom" required></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input placeholder="age" type="number" min="18" max="110" name="age" required></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input placeholder="pays" type="text" name="pays" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input id="id1" type="file" accept=".png" name="photo" ></td>
+                </tr>
+                <tr>
+                    <td><button id="inscription" type="button" onclick="Connect()">Se connecter</button></td>
+                    <td><button id="inscription" type="submit">S'inscrire</button></td>
+                </tr>
+            </table>
+        </legend>
+    </form>
+    <script>
+        function Accueil(){
+            window.location.href = "../page_connexion.php";
+        }
+        function Connect(){
+            window.location.href = "connexion.php";
+        }
+        function Password() {
+            password = document.getElementById("password");
+            if (password.type === "password")
+                password.type = "text";
+            else
+                password.type = "password";
+        }
+    </script>
+</body>
+</html>
---
+++ projet/php/user_list.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/user_list.css">
+        <title>Compte</title>
+    </head>
+    <body>
+        <div id=divdroit><a href="page_accueil.php"> <img id="home_icon" src="https://www.educol.net/coloriage-maison-dl28263.jpg" width="40px" alt="" ></a></div>
+        <p id="titre">Liste des utilisateurs</p>
+        <table id="table" border="solid">
+            <?php
+            $file_path = "../data/info_formulaire.json";
+            if(file_exists($file_path)){
+                $json_data = file_get_contents($file_path);
+                $tab = json_decode($json_data, true);
+                if(empty($json_data) || !is_array($tab)){
+                    echo "Erreur critique";
+                    exit();
+                }
+            }
+            else{
+                echo "Erreur Critique";
+                exit();
+            }
+            foreach($tab as $compte){
+                echo "<tr onclick='redirect(\"$compte[email]\")'>";
+                echo "<td class='border'>" . "<img class='img' src='../icones/" . $compte['photo'] . "'>" . "</td>";
+                echo "<td class='text'>" . $compte['email'] . "</td>";
+                echo "<td class='text'>" . $compte['password'] . "</td>";
+                echo "<td class='text'>" . $compte['prenom'] . "</td>";
+                echo "<td class='text'>" . $compte['nom'] . "</td>";
+                echo "<td class='text'>" . $compte['age'] . "</td>";
+                echo "<td class='text'>" . $compte['pays'] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+        <script>
+            function redirect(email){
+                var sessionEmail = "<?php echo $tab[$_SESSION['index']]['email']; ?>";
+                if(sessionEmail == email){
+                    window.location.href = "compte.php";
+                }
+                else{
+                    window.location.href = "chat.php";
+                }
+            }
+        </script>
+    </body>
+</html>
