5cf7c537dab920637b5ab02caf4d378463226cd7
Matmart12 <mathis.m7878@gmail.com>
2024-05-24 14:14:57 +0200

    tri entre les doublons

--- projet/php/abonné.php
+++ projet/php/abonné.php
@@ -1,12 +1,24 @@
-<?php
-session_start();
-echo "cool";
-$_SESSION["formule"]="abonné";
-$time=time();
-//changer la formule sur le fichier et faire en sorte que ce soit temporaire
-sleep(5);
-header("location:accueuil.php");
-?>
-
 <!DOCTYPE html>
 <html>
+<?php
+session_start();
+$file_path = "../data/info_formulaire.json";
+if(file_exists($file_path)){
+    $json_data = file_get_contents($file_path);
+    $tab = json_decode($json_data, true);
+    if(empty($json_data) || !is_array($tab)){
+        echo "Vous n'avez pas créer un compte sur Ship-Meeting";
+    }
+}
+$i=$_SESSION["index"];
+$Ug="abonné";
+$tab[$i]["grade"]=$Ug;
+$time=time();
+$tab[$i]["time"];
+file_put_contents($file_path, json_encode($tab,JSON_PRETTY_PRINT));
+header("location:accueuil.php");
+    
+    
+    sleep(5);
+    header("location:accueuil.php");
+?>
--- projet/php/admin.php
+++ projet/php/admin.php
@@ -1,38 +1,33 @@
+<!DOCTYPE html>
+<html>
 <?php
 session_start();
 ?>
+<head>
+<link rel="stylesheet" href="all.css">
+</head>
+<div id=divdroit><a href="accueuil.php"> <img src="https://www.educol.net/coloriage-maison-dl28263.jpg" width="40px" alt="" ></a></div>
+<form action="" method=post>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="all.css">
-    </head>
-    <body>
-        <div id=divdroit><a href="page_accueil.php"> <img src="https://www.educol.net/coloriage-maison-dl28263.jpg" width="40px" alt="" ></a></div>
-        <form action="" method=post>
-            <div id="table">
-            <center>
-                il semblerait que vous souhaitez devenir admin sur ce site. <br>
-                Veuillez donc trouver la solution du riddle suivant: <br>
-                <br>
-                -Riddle1 <br>
-                -Riddle2 <br>
-                -Riddle3 <br>
-                <br>
-                <textarea name="admin?" cols="50">
-                    Veuillez mettre votre réponse ici.
-                </textarea> <br>
-                <button onclick="ajoutadmin()">activer l'abonnement</button>
-            </center>
-            </div>
-        </form>
-    </body>
-</html>
+<div id="table">
+<center>
+        il semblerait que vous souhaitez devenir admin sur ce site. <br>
+        Veuillez donc trouver la solution du riddle suivant: <br>
+    <br>
+        -Riddle1 <br>
+        -Riddle2 <br>
+        -Riddle3 <br>
+        <br>
+    <textarea name="admin?" cols="50">
+        Veuillez mettre votre réponse ici.
+    </textarea> <br>
+    <button onclick="ajoutadmin()">activer l'abonnement</button>
+</center>
+</div>
+</form>
 <?php
 function ajoutadmin(){
     $_SESSION["formule"]="admin";
-    $time=time();
-    //changer la formule sur le fichier
+    header("location:accueuil.php");
 }
-?>
 
