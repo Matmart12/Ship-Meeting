2fedfe1d4b62cc96bc675aaf1425db82d25c190c
nismori <110066178+nismori@users.noreply.github.com>
2024-05-25 14:17:56 +0200

    Add files via upload

---
+++ projet/php/ad_admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="all.css">
+</head>
+
+</style>
+<?php
+session_start();
+$file_path = "../data/info_formulaire.json";
+if(file_exists($file_path)){
+    $json_data = file_get_contents($file_path);
+    $tab = json_decode($json_data, true);
+    if(empty($json_data) || !is_array($tab)){
+        echo "erreur crittique";
+    }
+}
+if($tab[$_SESSION["index"]]["grade"]!="abonné"&&$tab[$_SESSION["index"]]["grade"]!="inscrit"){
+    header("location:page_accueil.php");
+}
+$i=$_SESSION["index"];
+$Ug="admin";
+$tab[$i]["grade"]=$Ug;
+file_put_contents($file_path, json_encode($tab,JSON_PRETTY_PRINT));
+header("location:page_accueil.php");
+?>
--- projet/php/send_messages.php
+++ projet/php/send_messages.php
@@ -3,6 +3,7 @@ session_start();
 $file_path2 = "../data/info_formulaire.json";
 $file_path = "../data/send_messages.json";
 
+
 if (file_exists($file_path2)) {
     $json_data2 = file_get_contents($file_path2);
     $tab2 = json_decode($json_data2, true);
@@ -12,6 +13,7 @@ if (file_exists($file_path2)) {
 } else {
     $tab2 = array();
 }
+
 
 // Vérifiez si le fichier existe et chargez les données
 if (file_exists($file_path)) {
@@ -24,21 +26,23 @@ if (file_exists($file_path)) {
     $tab = array();
 }
 
+
 $email = $tab2[$_SESSION['index']]['email'];
 
-// Initialisez la variable de messages de session si elle n'existe pas
+
 if (!isset($_SESSION['send_messages'])) {
     $_SESSION['send_messages'] = array();
 }
 
-// Ajoutez le nouveau message à la session si reçu
+
 if (isset($_POST['message'])) {
     $_SESSION['send_messages'][] = $_POST['message'];
 }
 
+
 $last_entry = end($tab);
 
-// Rechercher l'entrée de l'utilisateur actuel dans le tableau
+
 $found = false;
 if ($last_entry['email'] == $email && $last_entry['other_email'] == $_SESSION['other_email']){
     $last_entry['messages'] = array_merge($last_entry['messages'], $_SESSION['send_messages']);
@@ -46,7 +50,7 @@ if ($last_entry['email'] == $email && $last_entry['other_email'] == $_SESSION['o
     $tab[key($tab)] = $last_entry;
 }
 
-// Si l'utilisateur n'existe pas encore dans le fichier, ajouter une nouvelle entrée
+
 if (!$found) {
     $data_array = array(
         'email' => $email,
@@ -56,8 +60,8 @@ if (!$found) {
     $tab[] = $data_array;
 }
 
-// Enregistrer les données mises à jour dans le fichier JSON
+
 file_put_contents($file_path, json_encode($tab, JSON_PRETTY_PRINT));
 
-// Réinitialisez les messages de la session après les avoir enregistrés
+
 $_SESSION['send_messages'] = array();
